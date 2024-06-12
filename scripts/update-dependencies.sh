b8514a30adfcf32748feee4dc71293afb6c691ab
Roman Sluchevskiy <roman.sluchevskii@threatrix.io>
2024-06-13 00:48:50 +0300

    cleanup

--- scripts/update-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -f build/updates.txt
-./gradlew dependencyUpdate -Drevision=release
-find . -name report.txt | xargs cat > build/updates.txt
-echo "Updates...."
-cat build/updates.txt | fgrep ' ->' | sort | uniq
